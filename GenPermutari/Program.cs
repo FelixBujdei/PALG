ff6dad0a0e7eb510e6bc9ae70bb2ac8a01bc5881
FelixBujdei <felix.bujdei@student.usv.ro>
2026-03-09 13:35:19 +0200

    Add files via upload

---
+++ GenPermutari/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        string caleFisier = @"D:\BujdeiFelix\GenPermutari\dateintrare.txt";
+        int n = CitesteDateDinFisier(caleFisier);
+
+        Stopwatch sw = Stopwatch.StartNew();
+        GenereazaPermutari(n);
+        sw.Stop();
+
+        Console.WriteLine($"\nTimp de executie: {sw.Elapsed.TotalMilliseconds} ms");
+    }
+
+    static int CitesteDateDinFisier(string numeFisier)
+    {
+        string continut = File.ReadAllText(numeFisier).Trim();
+        return int.Parse(continut);
+    }
+
+    static void GenereazaPermutari(int n)
+    {
+        int[] stiva = new int[n];
+        int k = 0;
+        stiva[k] = 0;
+
+        while (k >= 0)
+        {
+            bool amSuccesor = false;
+            bool esteValid = false;
+
+            do
+            {
+                if (stiva[k] < n)
+                {
+                    amSuccesor = true;
+                    stiva[k]++;
+
+                    esteValid = true;
+                    for (int i = 0; i < k; i++)
+                    {
+                        if (stiva[i] == stiva[k])
+                        {
+                            esteValid = false;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    amSuccesor = false;
+                }
+            } while (amSuccesor && !esteValid);
+
+            if (amSuccesor)
+            {
+                if (k == n - 1)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.Write(stiva[i]);
+                        if (i < n - 1) Console.Write(",");
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    k++;
+                    stiva[k] = 0;
+                }
+            }
+            else
+            {
+                k--;
+            }
+        }
+    }
+}
