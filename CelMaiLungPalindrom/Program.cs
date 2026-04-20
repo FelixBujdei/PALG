ec728a484281b65f1e52a082220724573479b2ad
FelixBujdei <felix.bujdei@student.usv.ro>
2026-04-20 13:23:52 +0300

    Add files via upload

---
+++ CelMaiLungPalindrom/Program.cs
@@ -0,0 +1,113 @@
+﻿class CelMaiLungPalindrom
+{
+    static void Main(string[] args)
+    {
+        new CelMaiLungPalindrom();
+    }
+    public CelMaiLungPalindrom()
+    {
+        GenerateTests();
+    }
+    /// <summary>
+    /// Genereaza teste pentru problema palindromului
+    /// </summary>
+    private void GenerateTests()
+    {
+        int[] n = new int[] {
+5, 10, 100, 300, 500, 700, 800, 900, 950, 999 };
+        for (int indexTest = 0; indexTest < n.Length; indexTest++)
+        {
+            int[] a = GenerateArray(n[indexTest]);
+            WriteToFile(a, "input-" + (indexTest + 1).ToString() + ".txt");
+            int start = Environment.TickCount;
+            using (StreamWriter writer = new StreamWriter(
+            "output-" + (indexTest + 1).ToString() + ".txt"))
+                writer.WriteLine(Solve(a));
+            int end = Environment.TickCount;
+            Console.WriteLine("time = {0} ms", end - start);
+        }
+    }
+    /// <summary>
+    /// Genereaza un sir de numere alese la intamplare
+    /// </summary>
+    Random random = new Random();
+    private int[] GenerateArray(int n)
+    {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = random.Next(100);
+        return a;
+    }
+    /// <summary>
+    /// Rezolva problema palindromulu
+    /// /// </summary>
+    private int Solve(int[] a)
+    {
+        int[] aReversed = new int[a.Length];
+        for (int i = 0; i < a.Length; i++)
+            aReversed[i] = a[a.Length - 1 - i];
+        return LengthOfLongestPalindrome(a, aReversed);
+    }
+    /// <summary>
+    /// Problema LCS
+    /// </summary>
+    private int LengthOfLongestPalindrome(int[] a, int[] b)
+    {
+        int n = a.Length;
+        int[,] length = new int[n, n];
+        length[0, 0] = (a[0] == b[0]) ? 1 : 0;
+        for (int j = 1; j < n; j++)
+            length[0, j] = (a[0] == b[j]) ? 1 : length[0, j - 1];
+        for (int i = 1; i < n; i++)
+            length[i, 0] = (a[i] == b[0]) ? 1 : length[i - 1, 0];
+        for (int i = 1; i < n; i++)
+            for (int j = 1; j < n; j++)
+                length[i, j] = max(
+                length[i - 1, j],
+                length[i, j - 1],
+                length[i - 1, j - 1] + (int)((a[i] == b[j]) ? 1 : 0));
+        return length[n - 1, n - 1];
+    }
+    private int max(int a, int b, int c)
+    {
+        return Math.Max(a, Math.Max(b, c));
+    }
+    /// <summary>
+    /// Afiseaza solutia la consola
+    /// </summary>
+    private void WriteToConsole(int[] a)
+    {
+        for (int i = 0; i < a.Length; i++)
+            Console.Write("{0}, ", a[i]);
+        Console.WriteLine();
+    }
+    /// <summary>
+    /// Afiseaza solutia intr-un fisier
+    /// </summary>
+    private void WriteToFile(int[] a, string fileName)
+    {
+
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            writer.WriteLine(a.Length);
+            for (int i = 0; i < a.Length; i++)
+                writer.Write("{0}{1}", a[i], i == a.Length - 1 ? "" : " ");
+        }
+    }
+    /// <summary>
+    /// Citeste datele problemei dintr-un fisier
+    /// </summary>
+    private int[] ReadFromFile(string fileName)
+    {
+        int[] a = null;
+        using (StreamReader reader = new StreamReader("int.txt"))
+        {
+            int n = int.Parse(reader.ReadLine());
+            string[] values = reader.ReadLine().Trim().Split(' ');
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = int.Parse(values[i]);
+        }
+        return a;
+    }
+}
---
+++ Veeificareavocabularului/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class VerificareVocabular
+{
+    static void Main()
+    {
+        CitesteSiRezolva("input_vocabular.txt", "output_vocabular.txt");
+    }
+
+    
+    static int DistantaLevenshtein(string cuvant1, string cuvant2)
+    {
+        int n = cuvant1.Length;
+        int m = cuvant2.Length;
+
+       
+        int[,] dp = new int[n + 1, m + 1];
+
+        
+        for (int i = 0; i <= n; i++)
+            dp[i, 0] = i;  
+
+        for (int j = 0; j <= m; j++)
+            dp[0, j] = j;  
+
+        
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (cuvant1[i - 1] == cuvant2[j - 1])
+                {
+                    
+                    dp[i, j] = dp[i - 1, j - 1];
+                }
+                else
+                {
+                   
+                    dp[i, j] = Math.Min(
+                        dp[i - 1, j] + 1,     
+                        Math.Min(
+                            dp[i, j - 1] + 1,  
+                            dp[i - 1, j - 1] + 1  
+                        )
+                    );
+                }
+            }
+        }
+
+        return dp[n, m];
+    }
+
+   
+    static string GasesteCuvantCorect(string cuvantGresit, List<string> dictionar)
+    {
+        string celMaiApropiat = cuvantGresit;
+        int distantaMinima = int.MaxValue;
+
+        foreach (string cuvantCorect in dictionar)
+        {
+            int distanta = DistantaLevenshtein(cuvantGresit, cuvantCorect);
+
+            if (distanta < distantaMinima)
+            {
+                distantaMinima = distanta;
+                celMaiApropiat = cuvantCorect;
+            }
+        }
+
+        return celMaiApropiat;
+    }
+
+    static void CitesteSiRezolva(string inputFile, string outputFile)
+    {
+        string[] linii = File.ReadAllLines(inputFile);
+        int index = 0;
+
+        
+        int n = int.Parse(linii[index++]);
+
+        
+        List<string> dictionar = new List<string>();
+        for (int i = 0; i < n; i++)
+        {
+            dictionar.Add(linii[index++].Trim());
+        }
+
+       
+        int m = int.Parse(linii[index++]);
+
+       
+        List<string> cuvinteUtilizator = new List<string>();
+        for (int i = 0; i < m; i++)
+        {
+            cuvinteUtilizator.Add(linii[index++].Trim());
+        }
+
+       
+        List<string> cuvinteCorectate = new List<string>();
+        foreach (string cuvant in cuvinteUtilizator)
+        {
+           
+            if (dictionar.Contains(cuvant))
+            {
+                cuvinteCorectate.Add(cuvant);
+            }
+            else
+            {
+              
+                string corectat = GasesteCuvantCorect(cuvant, dictionar);
+                cuvinteCorectate.Add(corectat);
+            }
+        }
+
+
+        using (StreamWriter writer = new StreamWriter(outputFile))
+        {
+            foreach (string cuvant in cuvinteCorectate)
+            {
+                writer.WriteLine(cuvant);
+            }
+        }
+        Console.WriteLine("Cuvinte corectate:");
+        foreach (string cuvant in cuvinteCorectate)
+        {
+            Console.WriteLine(cuvant);
+        }
+    }
+}
